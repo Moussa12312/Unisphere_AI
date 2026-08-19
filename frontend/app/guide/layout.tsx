6584cad3b101193615df64eb4dbfc514feca625f
Moussa Coulibaly <moucoulibaby111111@gmail.com>
2026-08-19 16:54:36 +0000

    feat: version finale avant deploiement production

--- frontend/app/guide/layout.tsx
+++ frontend/app/guide/layout.tsx
@@ -2,7 +2,7 @@ import RoleLayout from '@/components/layouts/RoleLayout';
 
 export default function TeacherLayout({ children }: { children: React.ReactNode }) {
   return (
-    <RoleLayout allowedRoles={['guard', 'admin']}>
+    <RoleLayout allowedRoles={['guard', 'admin', 'alumni', 'accountant', 'censeur', 'secretary', 'superadmin', 'teacher', 'student']}>
       {children}
     </RoleLayout>
   );
